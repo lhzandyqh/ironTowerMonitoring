be902916ad4db8032b51c300ff85cd33deef2cc0
ymq <king3410@qq.com>
2020-01-04 17:04:13 +0800

    fewf

--- build/utils.js
+++ build/utils.js
@@ -28,6 +28,12 @@ exports.cssLoaders = function (options) {
       sourceMap: options.sourceMap
     }
   }
+  const px2remLoader = {
+    loader: 'px2rem-loader',
+    options: {
+      remUnit: 37.5
+    }
+  }
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
@@ -41,8 +47,16 @@ exports.cssLoaders = function (options) {
         })
       })
     }
-
-    // Extract CSS when that option is specified
+    // const loaders = [cssLoader, px2remLoader]
+    // if (loader) {
+    //   loaders.push({
+    //     loader: loader + '-loader',
+    //     options: Object.assign({}, loaderOptions, {
+    //       sourceMap: options.sourceMap
+    //     })
+    //   })
+    // }
+      // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
       return ExtractTextPlugin.extract({
